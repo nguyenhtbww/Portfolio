088f60b176c4158be8bc6fa472278a0693a10ebe
nguyenhtbww <nguyenhieu24042004@gmail.com>
2025-07-03 15:32:50 +0700

    xong

--- src/components/main/Background.tsx
+++ src/components/main/Background.tsx
@@ -7,7 +7,7 @@ import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
 const Background = (props: any) => {
-  const ref: any = useRef();
+  const ref: any = useRef(null);
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
